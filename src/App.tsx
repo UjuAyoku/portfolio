5ad19943ea7a6e55e1b7aff8dbc151822c197eb2
UjuAyoku <63997970+UjuAyoku@users.noreply.github.com>
2025-10-31 14:03:41 -0600

    update portfolio

--- src/App.tsx
+++ src/App.tsx
@@ -19,10 +19,10 @@ const projects: Project[] = [
   {
     id: 1,
     title: "Cold Email Generator",
-    description: "Sales teams waste hours manually researching prospects. This AI tool automates personalized cold email generation by analyzing job listings, helping services companies accelerate their sales pipeline with context-aware emails optimized for conversion.",
+    description: "Accelerated B2B lead generation by automating context-aware email creation powered by AI and job listing analysis.",
     imageUrl: process.env.PUBLIC_URL + "/assets/email_generator.PNG",
     details:
-        ``,
+        "Sales teams waste hours manually researching prospects. This AI tool automates personalized cold email generation by analyzing job listings, helping services companies accelerate their sales pipeline with context-aware emails optimized for conversion.",
     tags: ["LLM", "LangChain", "Llama 3.1", "Groqcloud", "ChromaDB", "Python", "Streamlit", "Sales Automation"],
     githubUrl: "https://github.com/UjuAyoku/cold-email-generator/tree/main",
     demoUrl: "https://cold-email-creator.streamlit.app/",
@@ -31,10 +31,10 @@ const projects: Project[] = [
   {
     id: 2,
     title: "ARIA  - Audio Research & Intelligence Assistant",
-    description: "Tired of spending hours on research? ARIA is a fully automated research assistant built with n8n that turns any topic into a downloadable podcast-style audio briefing.",
+    description: "Transformed research efficiency by converting complex topics into digestible, on-demand audio insights.",
     imageUrl: process.env.PUBLIC_URL + "/assets/workflow-diagram.PNG",
     details:
-      "It sources data from academic databases, summarizes findings, and handles content moderation to deliver safe audio reports directly to your inbox, while logging any policy violation.",
+      "ARIA is an AI-powered research assistant built with n8n that automates topic research and converts summaries into podcast-style audio briefings for fast, hands-free knowledge consumption.",
     tags: ["n8n", "AI Agent", "Research", "OpenAI API", "Perplexity AI", "OpenAI Moderation", "Google Sheets API", "Open AI Text-To-Speech", "Workflow Automation"],
     githubUrl: "https://github.com/UjuAyoku/ARIA/tree/main",
     demoUrl: "https://n8n.research-ai-agent.com",
@@ -43,10 +43,10 @@ const projects: Project[] = [
     {
     id: 3,
     title: "Customer Churn Predictor",
-    description: "The Churn Predictor is a machine learning application that identifies customers at high risk of cancellation. By analyzing key factors like tenure, service type, and spending habits, it empowers businesses to proactively retain valuable customers with targeted outreach campaigns.",
+    description: "Enabled proactive customer retention through predictive analytics and data-driven outreach strategies.",
     imageUrl: process.env.PUBLIC_URL + "/assets/customer_churn.PNG",
     details:
-      "",
+      "The Churn Predictor identifies customers at high risk of cancellation by analyzing tenure, service type, and spending patterns; empowering businesses to take targeted action before churn occurs.",
     tags: ["EDA", "Python", "Scikit-learn", "Pandas", "Numpy", "Flask",  "JavaScript", "Machine Learning", "Predictive Modeling", ],
     githubUrl: "https://github.com/UjuAyoku/churn-predictor",
     demoUrl: "https://github.com/UjuAyoku/churn-predictor",
@@ -55,10 +55,10 @@ const projects: Project[] = [
   {
     id: 4,
     title: "SmartMoney",
-    description: "This web app helps you make smarter financial decisions. By computing the interests for investments, loans, or mortgage before signing on, you can make informed decisions about your finances.",
+    description: "Helped users make informed financial decisions through transparent computation of interest rates and loan outcomes.",
     imageUrl: process.env.PUBLIC_URL + "/assets/smartmoney.PNG",
     details:
-      "",
+      "SmartMoney is a financial web app that calculates potential investment returns, loan payments, and mortgage interest, promoting financial literacy and confidence before committing to major financial decisions.",
     tags: ["Python", "Flask", "Docker", "Tailwind CSS", "Google Cloud", "Railway", "Financial Technology"],
     githubUrl: "https://github.com/UjuAyoku/smart-money",
     demoUrl: "https://smartmoney-58502441680.europe-west1.run.app/",
@@ -68,10 +68,10 @@ const projects: Project[] = [
     id: 5,
     title: "Resume Optimizer",
     description:
-      "Looking for a new role? Use this app to create, analyze, and optimize your resume. You will receive an Applicant Tracking System (ATS) score and tailored suggestions of keywords required to maximize your chances.",
+      "Simplified job applications by offering AI-driven resume analysis and keyword optimization for creating tailored resumes for job postings.",
     imageUrl: process.env.PUBLIC_URL + "/assets/resume.PNG",
     details:
-      "",
+      "This app uses NLP and AI to analyze resumes against job descriptions, providing real-time keyword recommendations and an ATS compatibility score to help candidates stand out.",
     tags: ["AI", "NLP", "LLM", "OpenAI", "Python", "API", "CSS", "Streamlit Cloud", "ATS Optimization", "Resume Parsing"],
     githubUrl: "https://github.com/UjuAyoku/resume-optimizer/tree/main",
     demoUrl: "https://resumeoptimizerpro.streamlit.app/",
@@ -80,10 +80,10 @@ const projects: Project[] = [
   {
     id: 6,
     title: "Data Visualization",
-    description: "A collection of interactive visualizations built in Tableau.",
+    description: "Empowered decision-makers with intuitive, interactive dashboards that make business metrics actionable.",
     imageUrl: process.env.PUBLIC_URL + "/assets/data_viz.PNG",
     details:
-      "This collection features interactive dashboards that solve real-world problems across multiple domains. Each visualization leverages advanced techniques like parameters and LOD expressions to not only tell a story but also provide a dynamic tool for exploration and data-driven decision-making.",
+      "A collection of interactive Tableau and Power BI dashboards designed to visualize key business data, uncover insights, and support data-informed decisions across teams.",
     tags: ["Tableau", "Power BI", "Data Visualization", "Dashboard", "Data Analysis", "Business Intelligence"],
     githubUrl: "",
     demoUrl: "https://public.tableau.com/app/profile/uju.ayoku/vizzes",
@@ -153,7 +153,7 @@ const App: React.FC = () => {
         <div className="container mx-auto px-6 py-4 flex justify-between items-center">
           <h1
             className={`text-3xl font-bold ${
-              isDarkMode ? "text-purple-300" : "text-purple-800"
+              isDarkMode ? "text-purple-300" : "text-purple-900"
             }`}
           >
             Uju Ayoku
@@ -213,25 +213,25 @@ const App: React.FC = () => {
       <section className="container mx-auto px-6 py-16 text-center">
         <h2
           className={`text-5xl font-bold ${
-            isDarkMode ? "text-purple-300" : "text-purple-800"
+            isDarkMode ? "text-purple-300" : "text-purple-900"
           } mb-4`}
         >
           Hi,
         </h2>
         <h3
-          className={`text-xl ${
-            isDarkMode ? "text-purple-300" : "text-purple-800"
+          className={`max-w-5xl mx-auto text-justify ${
+            isDarkMode ? "text-purple-200" : "text-purple-900"
           } mb-4`}
         >
-          I am Uju, a Technical Program/Delivery Manager with technical experience in Generative AI, Deep Learning, Machine Learning, and Data Science.
+          I am Uju, a problem solver at heart, leading programs that bring structure to complexity and clarity to data. I manage cross-functional initiatives, 
+          build AI-powered solutions, and believe critical thinking is the ultimate competitive edge.
         </h3>
         <p  
-          className={`text-xl text-justify ${
-            isDarkMode ? "text-purple-200" : "text-purple-800"
+          className={`max-w-5xl mx-auto text-justify ${
+            isDarkMode ? "text-purple-200" : "text-purple-900"
           } mb-8`}
-        >
-          I leverage my hands-on experience in building ML solutions to effectively lead teams and deliver measurable impact. The projects below showcase this technical leadership, featuring work in data science and end-to-end AI/machine learning. 
-          If you are looking for someone who bridges technical depth with people-oriented leadership, you are in the right place. 
+        > If you are looking for someone who bridges technical depth with people-oriented leadership, you are in the right place. 
+        I am always open to conversations about project leadership, data-driven transformation and innovation.
         </p>
         <a
           href="#projects"
@@ -245,7 +245,7 @@ const App: React.FC = () => {
       <section id="projects" className="container mx-auto px-6 py-12">
         <h2
           className={`text-3xl font-bold ${
-            isDarkMode ? "text-purple-300" : "text-purple-800"
+            isDarkMode ? "text-purple-300" : "text-purple-900"
           } mb-8 text-center`}
         >
           Featured Projects
@@ -266,14 +266,14 @@ const App: React.FC = () => {
               <div className="p-6">
                 <h3
                   className={`text-xl font-semibold ${
-                    isDarkMode ? "text-purple-300" : "text-purple-800"
+                    isDarkMode ? "text-purple-300" : "text-purple-900"
                   } mb-2`}
                 >
                   {project.title}
                 </h3>
                 <p
                   className={`${
-                    isDarkMode ? "text-purple-200" : "text-purple-800"
+                    isDarkMode ? "text-purple-200" : "text-purple-900"
                   } mb-4`}
                 >
                   {project.description}
@@ -282,7 +282,7 @@ const App: React.FC = () => {
                   {project.tags.map((tag) => (
                     <span
                       key={tag}
-                      className="bg-purple-100 text-purple-800 text-sm px-2 py-1 rounded-full"
+                      className="bg-purple-100 text-purple-900 text-sm px-2 py-1 rounded-full"
                     >
                       {tag}
                     </span>
@@ -311,7 +311,7 @@ const App: React.FC = () => {
             <div className="flex justify-between items-center mb-4">
               <h3
                 className={`text-2xl font-bold ${
-                  isDarkMode ? "text-purple-300" : "text-purple-800"
+                  isDarkMode ? "text-purple-300" : "text-purple-900"
                 }`}
               >
                 {selectedProject.title}
@@ -321,7 +321,7 @@ const App: React.FC = () => {
                 className={`${
                   isDarkMode
                     ? "text-purple-300 hover:text-purple-100"
-                    : "text-purple-600 hover:text-purple-800"
+                    : "text-purple-600 hover:text-purple-900"
                 }`}
               >
                 <svg
@@ -384,27 +384,42 @@ const App: React.FC = () => {
         <div className="container mx-auto px-6">
           <h2
             className={`text-3xl font-bold ${
-              isDarkMode ? "text-purple-300" : "text-purple-800"
+              isDarkMode ? "text-purple-300" : "text-purple-900"
             } mb-8 text-center`}
           >
             About Me
           </h2>
-          <div className="max-w-2xl mx-auto text-justify">
+          <div className="max-w-5xl mx-auto text-justify">
             <p
               className={`${
-                isDarkMode ? "text-purple-200" : "text-purple-800"
+                isDarkMode ? "text-purple-200" : "text-purple-900"
               } mb-4`}
             >
-              I am a people-first leader who transforms complex initiatives in Engineering, IT, Data Science, Machine Learning, and Artificial Intelligence into measurable business value.
+              I am a Project and Program Manager who thrives at the intersection of technology, strategy, and execution. Over the past several years, I have led complex, 
+              multi-million-dollar initiatives, managing up to 16 concurrent projects and multiple sub-projects spanning diverse domains, time zones, and stakeholder groups.
               <br/><br/>
-              With a Master's in Data Science from Johns Hopkins, a Bachelor's in Electronics Engineering, and cross-industry experience spanning Oil & Gas, Software, Telecom, and Manufacturing 
-              across multiple continents, I bridge the gap between deep technical expertise and strategic execution.
-              <br/><br/>
-              As a certified PMP, SAFe Agile, PROSCI, and ITIL professional, I combine proven and program project leadership with hands-on technical skills to ensure advanced solutions are not only innovative, 
-              but also delivered on time, within scope, and aligned with business goals.
+              At {" "}
+                <a
+                  href="https://about.rogers.com/?icid=R_COR_JOR_OB4PMA"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-purple-700 hover:underline"
+                >
+                  Rogers
+                </a>, I manage two major capital programs where I ensure delivery excellence through meticulous budget oversight, contract alignment, and high-quality data 
+              management across Jira, Confluence, Big Picture, and  Oracle Fusion. I work seamlessly with internal teams, external vendors, and cross-functional stakeholders to keep projects on track, 
+              risks visible, and decisions data-informed.
               <br/><br/>              
-              When I am not tracking emerging technologies, you will likely find me hiking the trails of Southern Alberta or teaching kids robotics. I believe the best solutions come 
-              from curiosity, teamwork, and diverse perspectives.
+              Beyond execution, I am passionate about <em className="italic text-purple-800">driving clarity through complexity</em>. I combine strategic planning, stakeholder alignment, and financial acumen with 
+              technical fluency in <strong className="font-semibold text-purple-900">data science, machine learning, and AI</strong>, skills that not only inform my 
+              decision-making but also enable me to bridge business goals with cutting-edge innovation.
+              <br/><br/> 
+              I actively contribute to our Project Managers Community of Practice through mentorship, collaboration, and knowledge sharing. Whether I am orchestrating delivery across 
+              multiple streams or building end-to-end ML solutions in my own projects, I am motivated by the same purpose: <strong className="font-semibold text-purple-900">to deliver measurable value while keeping critical thinking and curiosity alive.</strong>
+              <br/><br/> 
+              Certified in Agile (Scrum, SAFe), PMP, Change management (PROSCI), Lean Portfolio Management, and ITIL, I bring a balance of analytical rigor, emotional 
+              intelligence, and a deep understanding of Agile and hybrid frameworks to every engagement. My focus is simple - enable teams, anticipate risks, manage change 
+              with empathy, and deliver results that stand up to data-driven scrutiny.
               </p>
           </div>
         </div>
@@ -414,11 +429,11 @@ const App: React.FC = () => {
       <section id="contact" className="container mx-auto px-6 py-16">
         <h2
           className={`text-3xl font-bold ${
-            isDarkMode ? "text-purple-300" : "text-purple-800"
+            isDarkMode ? "text-purple-300" : "text-purple-900"
           } mb-8 text-center`}
         >
           Get in Touch
-        </h2>
+        </h2>        
         <form onSubmit={handleSubmit} className="max-w-2xl mx-auto">
           <div className="mb-4">
             <label
